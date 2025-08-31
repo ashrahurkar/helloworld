d4d4846410883816a38876a2d74051f195f64637
Ashish R <ashrahurkar@yahoo.in>
2025-08-31 16:45:47 -0500

    Sample Tests

---
+++ src/test/java/com/example/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorldTest {
+    public void testHello(){
+        System.out.println("TEST - Hello...");
+    }
+
+    public void testWorld(){
+        System.out.println("TEST - World!!!");
+    }
+}
